9267cea90b1e157c97761ccc6e29ad7ead3a7965
FahimYNuryanto <fyusufnuryanto@gmail.com>
2022-06-06 04:55:45 +0900

    Polising some of react base

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Navbar, Container, Nav, Form, Button, NavDropdown, FormControl } from "react-bootstrap"
+import { Navbar, Container, Nav, Form, Button, FormControl } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/" >Feyn Minpro</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -13,19 +14,9 @@ const Header = () => {
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
+        <Nav.Link as={Link} to="/dashboard-admin">Dashboard Admin</Nav.Link>
+        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        <Nav.Link as={Link} to="/register">Register</Nav.Link>
       </Nav>
       <Form className="d-flex">
         <FormControl
@@ -34,7 +25,7 @@ const Header = () => {
           className="me-2"
           aria-label="Search"
         />
-        <Button variant="outline-success">Search</Button>
+        <Button variant="light">Search</Button>
       </Form>
     </Navbar.Collapse>
   </Container>
